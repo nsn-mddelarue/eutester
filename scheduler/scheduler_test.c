1fa381f2209fbacaa3f33e929fa0b262ab48090e
Michael De La Rue (Nokia Siemens Networks sp z o.o.) <git-krviq.nsn-mddelarue@spamgourmet.com>
2012-09-18 00:00:01 +0200

    First set of simple round robin scheduler tests

---
+++ scheduler/scheduler_test.c
@@ -0,0 +1,156 @@
+/* Copyright 2008 Mattias Norrby
+ * 
+ * Copyright (c) 2012  Nokia Siemens Networks Inc
+ * 
+ * This file is based on a file from Test Dept.
+ * 
+ * Eutester. is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Eutester. is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Test Dept..  If not, see <http://www.gnu.org/licenses/>.
+
+ * Author:
+ *   2008 Mattias Norrby
+ *   2012 Michael De La Rue on behalf of Nokia Siemens Networks Sp. z o.o.
+ *
+ */
+
+#include <test-dept.h>
+#include "handlers.h"
+#include <assert.h>
+
+/*
+ * the following are global variables within the Eucalyptus cluster controller
+ * each test which relies on them should set them up at the start of the test.
+ */
+
+ccConfig *config;
+ccResourceCache *resourceCache;
+
+/* given that I have a cloud which a resource in RESDOWN state
+ * when I attempt to schedule an instance with round_robin
+ * then it should not be scheduled on that node.
+ */ 
+
+
+int logprintfl(int level, const char *format, ...) {
+  return 1; 
+}
+
+int powerUp(ccResource *res) {
+  return 1; 
+}
+
+void setup_empty_cloud() {
+  ccResource *res;
+  int i;
+
+  config=malloc(sizeof(ccConfig));
+  assert(NULL != config );
+  /* FIXME: the following should be a macro both here and in main code */
+  config->schedState=0; 
+
+  resourceCache=malloc(sizeof(ccResourceCache));
+  assert(NULL != resourceCache );
+  for(i=0; i< MAXNODES; i++ ){
+    /* FIXME: the following should be a macro both here and in main code */
+    res=&(resourceCache->resources[0]);
+    res->state=RESDOWN;
+  }
+
+  for(i=0; i<2; i++) {
+    /* FIXME: the following should be macros both here and in main code */
+    res=&(resourceCache->resources[i]);
+    res->state=RESUP;
+    res->availMemory = 44;
+    res->availDisk = 88;
+    res->availCores = 8;
+  }
+
+}
+
+void setup_full_cloud() {
+  ccResource *res;
+  int i;
+
+  config=malloc(sizeof(ccConfig));
+  assert(NULL != config );
+  /* FIXME: the following should be a macro both here and in main code */
+  config->schedState=0; 
+
+  resourceCache=malloc(sizeof(ccResourceCache));
+  assert(NULL != resourceCache );
+  for(i=0; i< MAXNODES; i++ ){
+    /* FIXME: the following should be a macro both here and in main code */
+    res=&(resourceCache->resources[0]);
+    res->state=RESDOWN;
+  }
+  
+  for(i=0; i<2; i++) {
+    /* FIXME: the following should be macros both here and in main code */
+    res=&(resourceCache->resources[i]);
+    res->state=RESUP;
+    res->availMemory = 0;
+    res->availDisk = 7;
+    res->availCores = 0;
+  }
+
+}
+
+
+/* given that I have a cloud which has empty nodes with capacity for
+ * an instance when I attempt to schedule an instance with round_robin
+ * then it should be scheduled on the first node
+ */ 
+
+void test_empty_cloud_round_robin_scheduler() {
+  virtualMachine vm;
+  int outresid;
+  int ret;
+  ccResource *res;
+
+  setup_empty_cloud();
+  ret=schedule_instance_roundrobin(&vm, &outresid);
+
+  assert_equals(0, ret);
+
+  res=&(resourceCache->resources[0]);
+  
+  assert_equals(0, outresid);
+}
+
+/* given that I have a cloud which has no nodes with capacity for an instance
+ * when I attempt to schedule an instance with round_robin
+ * then I should get an error
+ */ 
+
+void test_full_cloud_round_robin_scheduler() {
+  virtualMachine vm;
+  int outresid;
+  int ret;
+  ccResource *res;
+
+  vm.mem = 5;
+  vm.disk = 5;
+  vm.cores = 5;
+  setup_full_cloud();
+  ret=schedule_instance_roundrobin(&vm, &outresid);
+
+  assert_not_equals(ret, 0);
+
+  res=&(resourceCache->resources[0]);
+  
+  assert_equals(0, outresid);
+}
+
+void teardown() {
+  ;
+}
